eba6bf8968600a4c4b78db56fa713403971ca224
kevzjhu <kevzjhu@gmail.com>
2026-07-16 21:37:05 -0400

    added: add transactions, edit, and delete. has better UI

--- transactions/views.py
+++ transactions/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TransactionForm
 from .models import Transaction
 
-# Create your views here.
 def home(request):
     if request.method == "POST":
-        form = TransactionForm(request.POST)
+        delete_id = request.POST.get('delete_id')
+        transaction_id = request.POST.get('transaction_id')
+        
+        # 1. Handle Delete Action
+        if delete_id:
+            transaction = get_object_or_404(Transaction, pk=delete_id)
+            transaction.delete()
+            return redirect('home')
+
+        # 2. Handle Add / Edit Actions
+        if transaction_id:
+            instance = get_object_or_404(Transaction, pk=transaction_id)
+            form = TransactionForm(request.POST, instance=instance)
+        else:
+            form = TransactionForm(request.POST)
+
         if form.is_valid():
             form.save()
             return redirect('home')
